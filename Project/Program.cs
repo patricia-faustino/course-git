dba22bb06e2900812909c3ff372e92d8d98aa045
Patricia Faustino <patriciafaustinobr@gmail.com>
2020-07-29 18:23:49 -0300

    Acrescentada mensagem de Bom Dia

--- Project/Program.cs
+++ Project/Program.cs
@@ -4,6 +4,10 @@ namespace Project {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia");
+
+            Console.ReadLine();
+
         }
     }
 }
